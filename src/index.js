8f283f735c7be3952ab370631cf485ca95bd738b
Ivan <lorf1991@gmail.com>
2021-10-26 02:24:17 +0300

    add

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import './sass/main.scss';
+import pictureMarkup from './templates/markup-pictures.hbs'
+import apiService from './js/apiService'
+import refs from './js/refs.js'
+
+const API = new apiService();
+
+refs.form.addEventListener('submit' , onSearchPicture);
+refs.loadMoreBtn.addEventListener('click' , onLoadMore);
+refs.gallery.addEventListener('click' , onClickPicture);
+
+function onSearchPicture (e){
+  e.preventDefault();
+  onClearMarkup();
+  API.value = refs.form.elements.query.value;
+  API.resetPage();
+  API.fetchImg().then(onRenderMarkup);
+  refs.form.reset();
+  refs.loadMoreBtn.style.display = 'block';
+}
+
+
+function onLoadMore (){
+  API.fetchImg().then(onRenderMarkup)
+  setTimeout(handleButtonClick,500)
+}
+
+function onRenderMarkup (imgCard){
+  const markup = pictureMarkup(imgCard);
+  refs.gallery.insertAdjacentHTML('beforeend' , markup);
+}
+
+function onClearMarkup () {
+  refs.gallery.innerHTML = '';
+}
+
+function handleButtonClick() {
+  const element = refs.gallery.lastElementChild;
+  element.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
+}
+
+function onClickPicture (e){
+  if(!e.target.classList.contains('picture')){
+    return
+  }
+  const elem = e.target.getAttribute('large-image');
+  onOpenModalWindow(elem)
+  onCloseModalWindow();
+}
+
+function onOpenModalWindow (data){
+  refs.modalWindow.classList.add('is-open');
+  refs.modalImage.src = data;
+}
+
+function onCloseModalWindow (){
+  refs.modalWindowOverlay.addEventListener('click' , () => {
+    refs.modalWindow.classList.remove('is-open')
+    refs.modalImage.src = '';
+  });
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      refs.modalWindow.classList.remove('is-open');
+      refs.modalImage.src = '';
+    }
+  })
+}
